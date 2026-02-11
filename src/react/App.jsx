5f12bf93b98ef6205d170a9d5d1d6326d7964435
catalinbroinas <catalinbroinas@yahoo.com>
2026-02-11 14:36:37 +0200

    feat(Game): add main component

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,7 +1,9 @@
+import Game from "./components/features/Game/Game";
 
 function App() {
   return (
     <div className="app-container">
+      <Game />
     </div>
   )
 }
---
+++ src/react/components/features/Game/Game.jsx
@@ -0,0 +1,10 @@
+
+function Game() {
+  return (
+    <div className="game">
+      <h1 className="game__title">Guess the number</h1>
+    </div>
+  );
+}
+
+export default Game;
