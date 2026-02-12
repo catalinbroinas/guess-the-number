14b99e64f51e4db4f1e049d8cf135414b296cdaf
catalinbroinas <catalinbroinas@yahoo.com>
2026-02-12 13:39:04 +0200

    refactor(Game): prepare structure for game logic

--- src/react/components/features/Game/Game.jsx
+++ src/react/components/features/Game/Game.jsx
@@ -1,19 +1,11 @@
 import GameSettings from "./GameSettings";
-import GuessInput from "./GuessInput";
-import Feedback from "./Feedback";
-import GameControls from "./GameControls";
 
 function Game() {
   return (
     <div className="game">
       <h1 className="game__title">Guess the number</h1>
 
-      <div className="mb-5">
-        <GameSettings />
-      </div>
-      <GuessInput />
-      <Feedback />
-      <GameControls />
+      <GameSettings />
     </div>
   );
 }
