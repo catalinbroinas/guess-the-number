fe7968f14165c2d1bd7c6804ccd396b7d4a606f5
catalinbroinas <catalinbroinas@yahoo.com>
2026-02-14 14:17:37 +0200

    feat(Game): validate guess and update game status

--- src/react/components/features/Game/Game.jsx
+++ src/react/components/features/Game/Game.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import GameSettings from "./GameSettings";
 import GuessInput from "./GuessInput";
 import { randomInt } from "../../../../js/utils/numberUtils";
+import Feedback from "./Feedback";
 
 function Game() {
   // State
@@ -20,15 +21,28 @@ function Game() {
     setGameStatus('playing');
   };
 
+  const handleGuess = (guess) => {
+    if (guess === secretNumber) {
+      setFeedbackMessage('Success!');
+      setGameStatus("won");
+    } else {
+      guess > secretNumber 
+        ? setFeedbackMessage('Too high.')
+        : setFeedbackMessage('Too low.');
+    }
+  };
+
   return (
     <div className="game">
       <h1 className="game__title">Guess the number</h1>
 
+      <Feedback message={feedbackMessage} />
+      
       {gameStatus === 'idle' && (
         <GameSettings onApply={handleApplyRange} />
       )}
       {gameStatus === 'playing' && (
-        <GuessInput onGuess={() => {}} />
+        <GuessInput onGuess={handleGuess} />
       )}
     </div>
   );
